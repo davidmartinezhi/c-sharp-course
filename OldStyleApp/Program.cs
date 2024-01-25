0da17770c1634ccf934f72ad6fa8aae3bba889ba
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-24 18:35:48 -0600

    Initial setup

---
+++ OldStyleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OldStyleApp;
+class Program
+{
+    // static es que podemos llamar el metodo sin instanciar la clase
+    // void es porque no regresa ningún valor
+    static void Main(string[] args) 
+    {
+        
+
+    }
+}
+
