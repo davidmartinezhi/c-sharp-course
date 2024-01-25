8a15fe7813cb362027a9a509a03333a778a888f2
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-25 17:11:48 -0600

    modularized lessons methods

--- OldStyleApp/Program.cs
+++ OldStyleApp/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace OldStyleApp;
+﻿using Microsoft.VisualBasic;
+
+namespace OldStyleApp;
 class Program
 {
     // static es que podemos llamar el metodo sin instanciar la clase
     // void es porque no regresa ningún valor
     static void Main(string[] args) 
     {
+        Conversions();
 
+    }
+
+    static void Variables()
+    {
         int num1; //this will be 0
 
         num1 = 13; //reassigned
@@ -26,6 +33,44 @@ class Program
 
 
         Console.ReadKey();
+    }
+
+    static void Conversions()
+    {
+
+        //implicit conversion
+        int num = 1223;
+        long bigNumber = num;
+        Console.WriteLine(bigNumber);
+
+        float myFloat = 13.37f;
+        double myNewDouble = myFloat;
+        Console.WriteLine(myNewDouble);
+
+        double myDouble = 13.37;
+        int myInt;
+
+        //explicit conversion
+        //cast double to int
+        myInt = (int)myDouble; //it does not round up, just leaves everything after dot
+
+
+        //type conversion
+        string myString = myDouble.ToString(); //converts to string
+        string myNumString = num.ToString();
+        string myFloatString = myFloat.ToString();
+
+
+        bool sunIsShining = false;
+        string boolToString = sunIsShining.ToString();
+
+        Console.WriteLine(myString);
+        Console.WriteLine(myNumString);
+        Console.WriteLine(myFloatString);
+        Console.WriteLine(boolToString);
+
+
+        Console.Read();
     }
 }
 
